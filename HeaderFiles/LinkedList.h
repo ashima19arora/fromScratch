30067603a7b755842295a1b20e339d8ad00a4656
Ashima Arora <arora.19ashima@gmail.com>
2026-09-14 19:00:00 +0530

    leetcode 206: reverse linked list - iterative and recursive

--- HeaderFiles/LinkedList.h
+++ HeaderFiles/LinkedList.h
@@ -255,3 +255,33 @@ ListNode<T>* midPoint03(ListNode<T>*& head) {
   }
   return slow;
 }
+
+// section 04: reversal of linked list
+
+// way 1 reversing the whole ll thru nodes , itertively
+ListNode<T>* reverseList(ListNode<T>* head) {
+  if (head == nullptr or head->next == nullptr) {
+    return head;
+  }
+  ListNode<T>* current = head;
+  ListNode<T>* prev = nullptr;
+  ListNode<T>* agla;
+  while (current != nullptr) {
+    agla = current->next;
+    current->next = prev;
+    prev = current;
+    current = agla;
+  }
+  return prev;
+}
+
+// way 2 reversing the whole ll thru nodes , recursively
+ListNode<T>* reverseList(ListNode<T>* head) {
+  if (head == nullptr or head->next == nullptr) {
+    return head;
+  }
+  ListNode<T>* reverseHead = reverseList(head->next);
+  head->next->next = head;
+  head->next = nullptr;
+  return reverseHead;
+}
